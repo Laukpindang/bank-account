3cee001e88300d9c1191a7879916b57f30f33fc8
laukpindang <ferdiansyahbandung3@gmail.com>
2022-09-08 17:03:31 +0700

    menghapus yg tak perlu

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,5 +11,3 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
-reportWebVitals();
